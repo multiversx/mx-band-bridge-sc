b34e51155ac312c422d90a69be884925e0f55d62
andrei-marinica <andrei.marinica@elrond.com>
2020-08-30 20:35:44 +0300

    level 1 & level 2 implementation

--- lvl1/src/lib.rs
+++ lvl1/src/lib.rs
@@ -6,6 +6,24 @@ imports!();
 #[elrond_wasm_derive::contract(BandBridgeLevel1Impl)]
 pub trait BandBridgeLevel1 {
 
-    
+    #[init]
+    fn init(&self) {
+    }
+
+    #[view(getPrice)]
+    #[storage_get("price")]
+    fn get_price_data(&self, symbol: Vec<u8>) -> Vec<u8>;
+
+    #[storage_set("price")]
+    fn set_price_data(&self, symbol: &[u8], price_data: &[u8]);
+
+    #[endpoint(updataPrice)]
+    fn update_price(&self, symbol: Vec<u8>, price_data: Vec<u8>) -> SCResult<()> {
+        require!(self.get_caller() == self.get_owner_address(), "only owner can update price");
+
+        self.set_price_data(symbol.as_slice(), price_data.as_slice());
+
+        Ok(())
+    }
     
 }
--- lvl2/src/lib.rs
+++ lvl2/src/lib.rs
@@ -3,9 +3,34 @@
 
 imports!();
 
+pub mod price_data;
+use price_data::*;
+
 #[elrond_wasm_derive::contract(BandBridgeLevel2Impl)]
 pub trait BandBridgeLevel2 {
 
-    
+    #[init]
+    fn init(&self) {
+    }
+
+    #[view(getPrice)]
+    #[storage_get("price")]
+    fn get_price_data(&self, symbol: Vec<u8>) -> PriceData;
+
+    #[storage_set("price")]
+    fn set_price_data(&self, symbol: &[u8], price_data: &PriceData);
+
+    #[endpoint(updataPrice)]
+    fn update_price(&self, symbol: Vec<u8>, price: u64, multiplier: u64) -> SCResult<()> {
+        require!(self.get_caller() == self.get_owner_address(), "only owner can update price");
+
+        self.set_price_data(symbol.as_slice(), &PriceData{
+            price,
+            multiplier,
+            last_update: self.get_block_nonce(),
+        });
+
+        Ok(())
+    }
     
 }
