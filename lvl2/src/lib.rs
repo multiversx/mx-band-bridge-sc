debd0beee226c823b13217b4b7f4061bf71ac785
andrei-marinica <andrei.marinica@elrond.com>
2020-09-28 10:55:20 +0300

    fixed uninitialized data case

--- lvl2/src/lib.rs
+++ lvl2/src/lib.rs
@@ -13,22 +13,28 @@ pub trait BandBridgeLevel2 {
     fn init(&self) {
     }
 
-    #[view(getPrice)]
     #[storage_get("price")]
-    fn get_price_data(&self, symbol: Vec<u8>) -> PriceData;
+    fn get_price_data(&self, symbol: Vec<u8>) -> Option<PriceData>;
 
     #[storage_set("price")]
-    fn set_price_data(&self, symbol: &[u8], price_data: &PriceData);
+    fn set_price_data(&self, symbol: &[u8], price_data: &Option<PriceData>);
+
+    /// Returns nothing (no result) if price data is not set.
+    /// Returns 1 result, the price data if it is set.
+    #[view(getPrice)]
+    fn get_price_data_endpoint(&self, symbol: Vec<u8>) -> OptionalResult<PriceData> {
+        self.get_price_data(symbol).into()
+    }
 
     #[endpoint(updataPrice)]
     fn update_price(&self, symbol: Vec<u8>, price: u64, multiplier: u64) -> SCResult<()> {
         require!(self.get_caller() == self.get_owner_address(), "only owner can update price");
 
-        self.set_price_data(symbol.as_slice(), &PriceData{
+        self.set_price_data(symbol.as_slice(), &Some(PriceData{
             price,
             multiplier,
             last_update: self.get_block_nonce(),
-        });
+        }));
 
         Ok(())
     }
